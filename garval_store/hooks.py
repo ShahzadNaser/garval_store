1cf9ee88f12793a10d272a6ae7f778934be2beb4
Shahzad Naser <shahzadnaser1122@gmail.com>
2026-01-20 11:44:19 +0500

    add basic setup files

--- garval_store/hooks.py
+++ garval_store/hooks.py
@@ -1,201 +1,100 @@
-from . import __version__ as app_version
-
 app_name = "garval_store"
 app_title = "Garval Store"
-app_publisher = "ShahzadNaser"
-app_description = "garval_store"
-app_email = "shahzadnaser1122@gmail.com"
+app_publisher = "Kashif Ali"
+app_description = "E-commerce platform with bilingual support (Spanish/English)"
+app_email = "kashif@example.com"
 app_license = "MIT"
 
-# Includes in <head>
-# ------------------
+# Fixtures - Custom Fields and Client Scripts
+fixtures = [
+    {
+        "dt": "Custom Field",
+        "filters": [["name", "in", ["User-email_verified", "User-email_verification_key"]]]
+    },
+    {
+        "dt": "Client Script",
+        "filters": [["name", "=", "Payment Request - Order Confirmation Button"]]
+    }
+]
 
-# include js, css files in header of desk.html
-# app_include_css = "/assets/garval_store/css/garval_store.css"
-# app_include_js = "/assets/garval_store/js/garval_store.js"
+# Website
+website_route_rules = [
+    {"from_route": "/home", "to_route": "home"},
+    {"from_route": "/about", "to_route": "about"},
+    {"from_route": "/contact", "to_route": "contact"},
+    {"from_route": "/shop", "to_route": "shop"},
+    {"from_route": "/product/<name>", "to_route": "product"},
+    {"from_route": "/cart", "to_route": "cart"},
+    {"from_route": "/checkout", "to_route": "checkout"},
+    {"from_route": "/order_confirmation", "to_route": "order_confirmation"},
+    {"from_route": "/my-account", "to_route": "my_account"},
+    {"from_route": "/customer-login", "to_route": "customer_login"},
+    {"from_route": "/customer-signup", "to_route": "customer_signup"},
+    {"from_route": "/verify-email", "to_route": "verify_email"},
+    {"from_route": "/payment", "to_route": "payment"},
+    {"from_route": "/payment-success", "to_route": "payment_success"},
+    {"from_route": "/payment-failed", "to_route": "payment_failed"},
+    {"from_route": "/politica-privacidad", "to_route": "politica_privacidad"},
+    {"from_route": "/aviso-legal", "to_route": "aviso_legal"},
+    {"from_route": "/politica-cookies", "to_route": "politica_cookies"},
+    {"from_route": "/declaracion-accesibilidad", "to_route": "declaracion_accesibilidad"},
+]
 
-# include js, css files in header of web template
-# web_include_css = "/assets/garval_store/css/garval_store.css"
-# web_include_js = "/assets/garval_store/js/garval_store.js"
+# Home page
+home_page = "home"
 
-# include custom scss in every website theme (without file extension ".scss")
-# website_theme_scss = "garval_store/public/scss/website"
+# App includes (for desk/backend - these will NOT affect /login)
+app_include_css = []
 
-# include js, css files in header of web form
-# webform_include_js = {"doctype": "public/js/doctype.js"}
-# webform_include_css = {"doctype": "public/css/doctype.css"}
+app_include_js = []
 
-# include js in page
-# page_js = {"page" : "public/js/file.js"}
+# Web includes (for website pages only)
+web_include_css = [
+    "/assets/garval_store/css/garval.css"
+]
 
-# include js in doctype views
-# doctype_js = {"doctype" : "public/js/doctype.js"}
-# doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
-# doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
-# doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
+web_include_js = [
+    "/assets/garval_store/js/garval.js"
+]
 
-# Home Pages
-# ----------
+# DocTypes
+# doc_events = {}
 
-# application home page (will override Website Settings)
-# home_page = "login"
+# On login hook - create Customer if not exists (for SSO users)
+on_login = "garval_store.user_hooks.on_user_login"
 
-# website user home page (by Role)
-# role_home_page = {
-#	"Role": "home_page"
-# }
-
-# Generators
-# ----------
-
-# automatically create page for each record of this doctype
-# website_generators = ["Web Page"]
-
-# Jinja
-# ----------
-
-# add methods and filters to jinja environment
-# jinja = {
-#	"methods": "garval_store.utils.jinja_methods",
-#	"filters": "garval_store.utils.jinja_filters"
-# }
-
-# Installation
-# ------------
-
-# before_install = "garval_store.install.before_install"
-# after_install = "garval_store.install.after_install"
-
-# Uninstallation
-# ------------
-
-# before_uninstall = "garval_store.uninstall.before_uninstall"
-# after_uninstall = "garval_store.uninstall.after_uninstall"
-
-# Desk Notifications
-# ------------------
-# See frappe.core.notifications.get_notification_config
-
-# notification_config = "garval_store.notifications.get_notification_config"
-
-# Permissions
-# -----------
-# Permissions evaluated in scripted ways
-
-# permission_query_conditions = {
-#	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
-# }
-#
-# has_permission = {
-#	"Event": "frappe.desk.doctype.event.event.has_permission",
-# }
-
-# DocType Class
-# ---------------
-# Override standard doctype classes
-
-# override_doctype_class = {
-#	"ToDo": "custom_app.overrides.CustomToDo"
-# }
-
-# Document Events
-# ---------------
-# Hook on document methods and events
-
-# doc_events = {
-#	"*": {
-#		"on_update": "method",
-#		"on_cancel": "method",
-#		"on_trash": "method"
-#	}
-# }
+# On session creation hook - run cart setup as Administrator to avoid permission errors
+on_session_creation = "garval_store.user_hooks.on_session_creation"
 
 # Scheduled Tasks
-# ---------------
+# scheduler_events = {}
 
-# scheduler_events = {
-#	"all": [
-#		"garval_store.tasks.all"
-#	],
-#	"daily": [
-#		"garval_store.tasks.daily"
-#	],
-#	"hourly": [
-#		"garval_store.tasks.hourly"
-#	],
-#	"weekly": [
-#		"garval_store.tasks.weekly"
-#	],
-#	"monthly": [
-#		"garval_store.tasks.monthly"
-#	],
-# }
+# Installation hooks
+after_install = "garval_store.install.after_install"
 
-# Testing
-# -------
+# Website context
+website_context = {
+    "favicon": "/assets/garval_store/images/favicon.ico",
+    "splash_image": "/assets/garval_store/images/logo.png"
+}
 
-# before_tests = "garval_store.install.before_tests"
+# Update website context to conditionally exclude CSS from Frappe login/signup pages
+update_website_context = "garval_store.utils.update_website_context"
 
-# Overriding Methods
-# ------------------------------
-#
-# override_whitelisted_methods = {
-#	"frappe.desk.doctype.event.event.get_events": "garval_store.event.get_events"
-# }
-#
-# each overriding function accepts a `data` argument;
-# generated from the base implementation of the doctype dashboard,
-# along with any modifications made in other Frappe apps
-# override_doctype_dashboards = {
-#	"Task": "garval_store.task.get_dashboard_data"
-# }
+# Jinja environment customizations
+jinja = {
+    "methods": [
+        "garval_store.utils.get_lang"
+    ]
+}
 
-# exempt linked doctypes from being automatically cancelled
-#
-# auto_cancel_exempted_doctypes = ["Auto Repeat"]
+# Override whitelisted methods
+override_whitelisted_methods = {}
 
-# Ignore links to specified DocTypes when deleting documents
-# -----------------------------------------------------------
+# Override doctype classes
+override_doctype_class = {
+	"Customer": "garval_store.overrides.customer.Customer",
+}
 
-# ignore_links_on_delete = ["Communication", "ToDo"]
-
-# Request Events
-# ----------------
-# before_request = ["garval_store.utils.before_request"]
-# after_request = ["garval_store.utils.after_request"]
-
-# Job Events
-# ----------
-# before_job = ["garval_store.utils.before_job"]
-# after_job = ["garval_store.utils.after_job"]
-
-# User Data Protection
-# --------------------
-
-# user_data_fields = [
-#	{
-#		"doctype": "{doctype_1}",
-#		"filter_by": "{filter_by}",
-#		"redact_fields": ["{field_1}", "{field_2}"],
-#		"partial": 1,
-#	},
-#	{
-#		"doctype": "{doctype_2}",
-#		"filter_by": "{filter_by}",
-#		"partial": 1,
-#	},
-#	{
-#		"doctype": "{doctype_3}",
-#		"strict": False,
-#	},
-#	{
-#		"doctype": "{doctype_4}"
-#	}
-# ]
-
-# Authentication and authorization
-# --------------------------------
-
-# auth_hooks = [
-#	"garval_store.auth.validate"
-# ]
+# Default language
+default_language = "es"
